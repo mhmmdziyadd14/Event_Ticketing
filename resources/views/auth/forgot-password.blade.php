6bf2a568cc163eb4c3f0bfeda4863a103f868dd8
Nazwa Nur Salsa Bella <nazwa.nur@mhs.itenas.ac.id>
2025-01-05 18:24:20 +0700

    kata kata

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -7,46 +7,7 @@
     <title>Reset Password</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"> <!-- Font Awesome CDN -->
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            height: 100vh;
-            background-image: url("https://i.pinimg.com/736x/63/9d/0f/639d0fb393636750349b0f6708fe9712.jpg");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-        }
-
-        .reset-container {
-            max-width: 400px;
-            padding: 2rem;
-            background-color: rgba(255, 255, 255, 0.75);
-            border-radius: 1rem;
-            box-shadow: 0 3px 15px rgba(0, 0, 0, 0.1);
-            transition: all 0.3s ease-in-out;
-        }
-
-        .reset-container:hover {
-            box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
-            transform: translateY(-5px);
-        }
-
-        .primary-button {
-            background-color: #46498d;
-            color: rgb(249, 249, 250);
-            border: none;
-            padding: 0.80rem 2rem;
-            border-radius: 50px;
-            transition: background-color 0.3s ease-in-out, transform 0.3s ease-in-out;
-            width: 100%;
-        }
-
-        .primary-button:hover {
-            background-color: #1E3A8A;
-            transform: scale(1.05);
-        }
-    </style>
+    <link rel="stylesheet" href="css/forgot.css">
 </head>
 <body>
     <div class="d-flex align-items-center justify-content-center min-vh-100">
